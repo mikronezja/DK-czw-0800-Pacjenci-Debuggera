943b475ec39a8d8951d41e5255b3170999325ffa
a-jednak <zjedynak@student.agh.edu.pl>
2025-12-17 21:58:14 +0100

    Small test fix

--- clinic/src/test/java/com/oot/clinic/ClinicApplicationTests.java
+++ clinic/src/test/java/com/oot/clinic/ClinicApplicationTests.java
@@ -9,13 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@Profile("test")
+@ActiveProfiles("test")
 class ClinicApplicationTests {
 
 	@Autowired
